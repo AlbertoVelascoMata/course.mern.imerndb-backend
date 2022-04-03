d1b727656b4cefdfb4a7dfd5f222c24fef6d7784
AlbertoVelascoMata <Alberto.Velasco1@alu.uclm.es>
2022-04-03 20:43:42 +0200

    add frontend link in index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'IMernDB backend',
+        frontend_uri: 'https://albertovelascomata.github.io/course.mern.imerndb-frontend/'
+    });
 });
 
 module.exports = router;
